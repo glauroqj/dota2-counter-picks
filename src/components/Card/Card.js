cf7faf65b2d128c8667cb5cafa03b97bcc4fa2cc
glauroqj <glauro.juliani@hotmail.com>
2020-10-06 19:19:33 -0300

    add: style to card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 /** style */
 import * as El from './Card.style'
 
-const Card = () => {
+const Card = ({
+  image
+}) => {
 
   return(
     <El.CardContainer>
       <El.CardImageContainer>
-
+        <img loading='lazy' src='https://www.pcgamesn.com/wp-content/uploads/2019/10/best-dota-2-heroes-lich-loading-900x506.jpg' alt='' title='' />
       </El.CardImageContainer>
-      
+
+      <El.CardBodyCotnainer>
+        BODY
+      </El.CardBodyCotnainer>
     </El.CardContainer>
   )
+}
+
+Card.defaultProps = {
+  image: ''
+}
+Card.propTypes = {
+  image: PropTypes.string
 }
 
 export default Card
--- src/components/Card/Card.style.js
+++ src/components/Card/Card.style.js
@@ -2,7 +2,27 @@ import styled from 'styled-components'
 
 export const CardContainer = styled.div`
   display: flex;
-  border: 1px solid ${props => props.theme.color.primary_light};
+  flex-flow: column nowrap;
+  border: 1px solid ${props => props.theme.color.primary};
+  border-radius: ${props => props.theme.space[4]};
 `
 
-export const CardImageContainer = styled.picture``
+export const CardImageContainer = styled.picture`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+
+  > img {
+    width: 50%;
+    height: auto;
+    -webkit-border-top-left-radius: ${props => props.theme.space[4]};
+    -webkit-border-top-right-radius: ${props => props.theme.space[4]};
+    -moz-border-radius-topleft: ${props => props.theme.space[4]};
+    -moz-border-radius-topright: ${props => props.theme.space[4]};
+    border-top-left-radius: ${props => props.theme.space[4]};
+    border-top-right-radius: ${props => props.theme.space[4]};
+  }
+`
+export const CardBodyCotnainer = styled.div`
+  padding: ${props => props.theme.space[12]};
+`
